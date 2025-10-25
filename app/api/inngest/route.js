ce38d78a14712fefb1a55218f68e2c40e31eddfd
sreepriyakmo4 <147272656+sreepriyakmo4@users.noreply.github.com>
2025-10-25 16:54:30 +0530

    added api

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,9 @@
+
 import { serve } from "inngest/next";
 import { inngest } from "@lib/inngest/client";
-import { helloWorld } from "@lib/inngest/functions";
+import { generateIndustryInsights } from "@lib/inngest/functions";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [ 
-    helloWorld,
-
-  ] ,
+  functions: [generateIndustryInsights],
 });
